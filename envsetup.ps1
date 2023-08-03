42b1a130f4b22f3effc8b28d30e5f028e1bf036f
Zeno Muscarella <zmuscarella@gmail.com>
2023-08-02 21:57:46 -0700

    lazy upload

---
+++ envsetup.ps1
@@ -0,0 +1,19 @@
+# create python3.9 virtual env
+py -3.9 -m venv venv
+# activate the powershell venv
+venv\Scripts\activate.ps1
+# if requirements.txt file exists then pip install it other wise install and freeze
+if (Test-Path requirements.txt) {
+    pip install -r requirements.txt
+} else {
+    # install gradio package
+    pip install gradio
+    # eventually we'll intsall some other stuff too
+    # install other stuff!!
+    # done installing everything!
+    # pip freeze environment
+    pip freeze > requirements.txt
+}
+
+
+
